efd33cd06c439b81e51a6cdd79d6f4b15c8b10a8
KirillFedotenko <159914000+KirillFedotenko@users.noreply.github.com>
2024-02-15 18:18:59 +0300

    Add files via upload

---
+++ 15.02.2024/15.02.2024.cpp
@@ -0,0 +1,148 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	//int size;
+	//double dist;
+	//cout << "Масштаб карты =" << endl;
+	//cin >> size;
+	//cout << "Рсстояние между точками, изображающими населенные пункты =" << endl;
+	//cin >> dist;
+	//cout <<"Расстояние между населенными пунктами =" << Size * dist << endl;
+
+
+
+
+	//int num, num1, num2, num3;
+	//	cout << "Ведите 3-х значное число:";
+	//cin >> num;
+	//num1 = num / 100 % 10;
+	//num2 = num / 10 % 10*10;
+	//num3 = num % 10*100;
+	//cout << num1+num2+num3 << endl;
+
+
+
+	//int a, b, c;
+	//cout << "Вычисление параллелепипеда." << endl;
+	//cout << "Введите длину в см:\t" << endl;
+	//double a;
+	//cout << "Введите ширину в см:\t" << endl;
+	//double b;
+	//cout << "Введите высоту в см:\t" << endl;
+	//double c;
+	//cout << "Объем = " << a * b * c;
+
+
+
+
+	//int num;
+	//cin >> num;
+	//if (num % 2 == 0) {
+	//	cout << "Четное" << endl;
+	//}
+	//else {
+	//	cout << "Не четное";
+	//}
+
+
+	//int num, num1;
+	//cin >> num;
+	//cin >> num1;
+	//if (num > num1) {
+	//	cout << num;
+	//}
+	//else {
+	//	cout << num1;
+	//}
+	//cout << "" << endl;
+	//cout << ((num > num1) ? num : num1);
+
+
+
+
+
+	//int num;
+	//cin >> num;
+	//if (num < 0) {
+	//	cout << "Отрицательное";
+	//}
+	//else if (num>0){
+	//	cout << "Положительное";
+	//}
+	//else {
+	//	cout << "Равно нулю";
+	//}
+
+
+
+	//int num, num1;
+	//cin >> num;
+	//cin >> num1;
+	//if (num > num1) {
+	//	cout << num << " " << num1;
+	//}
+	//else if(num1>num){
+	//	cout << num <<" " << num1;
+	//}
+	//else {
+	//	cout << "они равны";
+	//}
+
+
+
+	//int num, num1,num2,num3 ,num4,summa;
+	//cin >> num;
+	//cin >> num1;
+	//cin >> num2;
+	//cin >> num3;
+	//cin >> num4;
+	//summa = num + num1 + num2 + num3 + num4;
+	//if (summa / 5 >=4) {
+	//	cout << "Ученик допущен";
+	//}
+	//else {
+	//	cout << "Ученик отчислен";
+	//}
+
+
+
+
+
+
+	//int num;
+	//cin >> num;
+	//if (num % 2 == 0) {
+	//	cout << num*3;
+	//}
+	//else if(num%2!=0) {
+	//	cout << num/2.0;
+	//}
+
+
+
+
+
+	double num1, num2;
+	char action;
+	cin >> num1;
+	cin >> num2; 
+		cin << action;
+	if (action == '+') {
+		cout << num1 + num2;
+	}
+	else if (action == '-') {
+		cout << num1 - num2;
+	}
+	else if (action == '*') {
+		cout << num1 * num2;
+	}
+	else if (action == '/' {
+		cout << num1 / num2;
+	}
+
+}
