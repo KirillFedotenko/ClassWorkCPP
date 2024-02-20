6fed2be6cc7c824aca2390f74590cf62fc2571d9
KirillFedotenko <159914000+KirillFedotenko@users.noreply.github.com>
2024-02-20 18:20:48 +0300

    Add files via upload

---
+++ 20.02.2024/20.02.2024.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	/*double num;
+	double num1;
+	cin >> num;
+	cin >> num1;
+	char op;
+	cin >> op;*/
+	//if (num == 0)
+	//	cout << "zero";
+	//else if (num == 1)
+	//	cout << "one";
+	//else if (num == 2)
+	//	cout << "two";
+	//else if (num == 3)
+	//	cout << "three";
+	//else
+	//	cout << "another num";
+
+	//switch (num) {
+	//case 1:
+	//case 12:
+	//case 2:
+	//	cout << "winter";
+	//	break;
+	//case 3:
+	//case 4:
+	//case 5:
+	//	cout << "spring";
+	//	break;
+	//case 6:
+	//case 7:
+	//case 8:
+	//	cout << "SUMMER";
+	//	break;
+	//case 9:
+	//case 10:
+	//case 11:
+	//	cout << "winter";
+	//	break;
+
+
+	/*switch (op) {
+	case '+':
+		cout << num << "+" << num1 << "=" << num + num1;
+		break;
+	case '-':
+		cout << num << "-" << num1 << "=" << num - num1;
+		break;
+	case '*':
+		cout << num << "*" << num1 << "=" << num * num1;
+		break;
+	case '/':
+		if (num1 == 0)
+			cout << "error";
+		else
+			cout << num << "/" << num1 << "/" << num * num1;
+		break;*/
+
+
+
+
+
+	//int num;
+	//int st2;
+	//int fn2;
+	//int st3;
+	//int fn3;
+	//cin >> num;
+	//if (num % 2 == 0)
+	//	cout << "четное" << endl;
+	//else
+	//	cout << "нечет" << endl;
+
+
+
+	//cout << "введите начало и конец 2ого диапозона" << endl;
+	//cin >> st2;
+	//cin >> fn2;
+
+	//cout << "введите начало и конец 3ого диапозона" << endl;
+	//cin >> st3;
+	//cin >> fn3;
+
+
+
+	//if (num >= 10 && num <= 20)
+	//	cout << "число входит в 1 диапозон" << endl;
+	//else if (num >= st2 && num <= st2)
+	//	cout << "число входит в 2ой диапозон" << endl;
+	//else if (num >= st3 && num <= st3)
+	//	cout << "число входит в 3ий диапозон" << endl;
+	//else
+	//	cout << "не входит";
+
+	int c1;
+	int c2;
+	int c3;
+	
+	
+}
+
+
