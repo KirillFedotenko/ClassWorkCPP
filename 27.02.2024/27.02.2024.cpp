cb2d6d5d0f78e502f9680cb22491eb4118cd00b1
KirillFedotenko <159914000+KirillFedotenko@users.noreply.github.com>
2024-02-27 18:15:39 +0300

    Add files via upload

---
+++ 27.02.2024/27.02.2024.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    //int a, sum;
+    //  sum = 1;
+    //  cin >> a;
+    //  if (a < 0) {
+    //      cout << "ERROR";
+    //  }
+    //  while (a > 1) {
+    //      sum * a--;
+    //  }
+    //  cout << sum;
+
+
+
+      //int a, b, c;
+      //cout << "введите границы диапозона:" << endl;
+      //cin >> a >> b;
+      //if (a > b) swap(a, b);
+      //c = a;
+      //cout << "Все значения:" << endl;
+      //while (c <= b) {
+      //    cout << c++ << " ";
+      //}
+      //cout << " " << endl;
+      //cout << "Эти значения из диапозона:" << a << " " << b;
+
+
+
+      //int n;
+      //n = 100;
+      //for (int i = 0; i < n; i+=2) {
+      //    cout << i << " ";
+      //}
+      //cout << "100";
+
+
+      /*int a, b = 0;
+      cin >> a;
+      for(int i = a; i != 0; i /= 10) {
+          b *= 10;
+          b += i % 10;
+      }
+      cout << b;*/
+
+      //int a, b;
+      //char c;
+      //cin >> c;
+      //cin >> a;
+      //cin >> b;
+      //for (int i = 0; i < a; i++) {
+      //    for (int j = 0; j < b; j++) {
+      //        if (i == 0 || i == a - 1 || j == 0 || j == b - 1)cout << c << " ";
+      //        else cout << "  ";
+      //    }
+      //    cout << endl;
+      //}
+
+
+
+//    int n = 0;
+//    int a = 0;
+//    int b = 0;
+//    cin >> n;
+//    cin >> a;
+//    for (int i = 0; i < n; i++)
+//    {
+//        for (int j = 0; j <= b; j++)
+//            cout << "* ";
+//        b++;
+//        cout << endl;
+//    }
+}  
