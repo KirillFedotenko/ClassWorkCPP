6a0a23967eb4fb158a003d3039ac3acf15a67931
KirillFedotenko <159914000+KirillFedotenko@users.noreply.github.com>
2024-10-10 21:05:57 +0300

    Add files via upload

---
+++ 10.10.2024/10.10.2024.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+//union Biba 
+//{
+//	int a;
+//	unsigned char bytes[4];
+//	void show() {
+//		for (int i = 0; i < 4; i++) {
+//			cout << int (bytes[i]);
+//		}
+//	}
+//};
+
+//union Number {
+//	int a;
+//	unsigned int b;
+//};
+
+union Three
+{
+	int a;
+	bool arr[];
+
+};
+
+int main()
+{
+	//Biba aboba;
+	//aboba.a = 5;
+	//aboba.show();
+	
+	//Number popa.a = 3;
+
+
+}
